4ac927174852aee8bd2b6bb15b6bd10d0d4cdb03
martinBogdanov1 <martin.petrov1994@yahoo.com>
2021-08-08 19:08:08 +0300

    removed unused import

--- bandshare/src/app/content/content.service.ts
+++ bandshare/src/app/content/content.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http'
 import { environment } from 'src/environments/environment';
 import { IBand } from '../shared/interfaces/band';
 import { IBands } from '../shared/interfaces/bands';
-import { map } from 'rxjs/operators';
 
 const API_URL = environment.API_URL;
 const headers = environment.headers;
@@ -14,31 +13,31 @@ export class ContentService {
   constructor(private http: HttpClient) { }
 
   getAllBands() {
-    return this.http.get<IBands>(`${API_URL}/posts`, {
+    return this.http.get<IBands>(`${API_URL}/classes/posts`, {
       headers: headers
     });
   }
 
   getBandById(bandId: string) {
-    return this.http.get<IBand>(`${API_URL}/posts/${bandId}`, {
+    return this.http.get<IBand>(`${API_URL}/classes/posts/${bandId}`, {
       headers: headers
     });
   }
 
   createBand(data: Object) {
-    return this.http.post<IBand>(`${API_URL}/posts`, data, {
+    return this.http.post<IBand>(`${API_URL}/classes/posts`, data, {
       headers
     });
   }
 
   deleteBand(bandId: string,) {
-    return this.http.delete(`${API_URL}/posts/${bandId}`, {
+    return this.http.delete(`${API_URL}/classes/posts/${bandId}`, {
       headers: headers
     });
   }
 
   editBand(bandId: string, data: Object) {
-    return this.http.put(`${API_URL}/posts/${bandId}`, data, {
+    return this.http.put(`${API_URL}/classes/posts/${bandId}`, data, {
       headers: headers
     });
   }
