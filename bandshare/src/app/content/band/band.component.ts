3c8ce35a1a2bd591c6ff865bdd0056fde027d953
martinBogdanov1 <martin.petrov1994@yahoo.com>
2021-08-10 22:33:19 +0300

    add conditional rendering for form inputs and buttons

--- bandshare/src/app/content/band/band.component.ts
+++ bandshare/src/app/content/band/band.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'src/app/auth/user.service';
 import { IBand } from 'src/app/shared/interfaces';
 import { ContentService } from '../content.service';
 
@@ -11,10 +12,15 @@ import { ContentService } from '../content.service';
 export class BandComponent implements OnInit {
   band: IBand | undefined;
 
+  get currentUser() {
+    return this.userService.currentUser;
+  }
+
   constructor(
     private contentService: ContentService,
     private activatedRoute: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private userService: UserService
   ) { }
 
   ngOnInit(): void {
