17f29297f06765b44c45ecc4f924223f57edb996
martinBogdanov1 <martin.petrov1994@yahoo.com>
2021-08-10 20:54:31 +0300

    hide edit/delete buttons for logged out user

--- bandshare/src/app/content/catalog/catalog.component.ts
+++ bandshare/src/app/content/catalog/catalog.component.ts
@@ -1,31 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
 import { IBands } from 'src/app/shared/interfaces';
 import { ContentService } from '../content.service';
+import { UserService } from 'src/app/auth/user.service';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.scss'],
 })
-export class CatalogComponent implements OnInit {
+export class CatalogComponent {
   bands: IBands | undefined;
+
+  get currentUser() {
+    return this.userServce.currentUser;
+  }
 
   constructor(
     private contentSerice: ContentService,
-    private router: Router
+    private userServce: UserService
   ) {
-
+    this.getBands();
   }
 
-  ngOnInit(): void {
+  getBands(): void {
+    this.bands = undefined;
     this.contentSerice.getAllBands().subscribe(content => this.bands = content);
   }
+
 
   delete(band: HTMLDivElement, id: string) {
     this.contentSerice.deleteBand(id).subscribe({
       next: () => {
-        band.remove;
+        band.remove();
+        this.getBands();
       },
       error: (err) => {
         return;
